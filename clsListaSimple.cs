5d351eb19d6288bd2562e064146487d53736a5c5
root <mgiraudo@ies21.edu.ar>
2024-04-10 10:24:50 -0300

    10:24 10/4

--- clsListaSimple.cs
+++ clsListaSimple.cs
@@ -46,5 +46,74 @@ namespace pryProyecto
                 }
             }
         }
+
+        public void Eliminar(Int32 Codigo)
+        {
+            if(Primero.Codigo == Codigo)
+            {
+                Primero = Primero.Siguiente;
+            }
+            else
+            {
+                clsNodo Ant = Primero;
+                clsNodo Aux = Primero;
+                while(Aux.Codigo != Codigo)
+                {
+                    Ant = Aux;
+                    Aux = Aux.Siguiente;
+                }
+                Ant.Siguiente = Aux.Siguiente;
+            }
+        }
+
+        public void Recorrer(DataGridView Grilla)
+        {
+            clsNodo aux = Primero;
+            Grilla.Rows.Clear();
+            while (aux != null)
+            {
+                Grilla.Rows.Add(aux.Codigo, aux.Nombre, aux.Tramite);
+                aux = aux.Siguiente;
+            }
+        }
+
+        public void Recorrer(ListBox Lista)
+        {
+            clsNodo aux = Primero;
+            Lista.Items.Clear();
+            while (aux != null)
+            {
+                Lista.Items.Add(aux.Codigo);
+                aux = aux.Siguiente;
+            }
+        }
+
+        public void Recorrer(ComboBox Combo)
+        {
+            clsNodo aux = Primero;
+            Combo.Items.Clear();
+            while (aux != null)
+            {
+                Combo.Items.Add(aux.Nombre);
+                aux = aux.Siguiente;
+            }
+        }
+        public void Recorrer()
+        {
+            clsNodo aux = Primero;
+            StreamWriter AD = new StreamWriter("Lista.csv", false, Encoding.UTF8);
+            AD.WriteLine("Lista de espera\n");
+            AD.WriteLine("Código;Nombre;Trámite");
+            while (aux != null)
+            {
+                AD.Write(aux.Codigo);
+                AD.Write(";");
+                AD.Write(aux.Nombre);
+                AD.Write(";");
+                AD.WriteLine(aux.Tramite);
+                aux = aux.Siguiente;
+            }
+            AD.Close();
+        }
     }
 }
--- frmListaSimple.Designer.cs
+++ frmListaSimple.Designer.cs
@@ -29,12 +29,9 @@ namespace pryProyecto
         /// </summary>
         private void InitializeComponent()
         {
-            this.lstCola = new System.Windows.Forms.ListBox();
-            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.dgvCola = new System.Windows.Forms.DataGridView();
-            this.Tramite = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.lstLista = new System.Windows.Forms.ListBox();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.cboxLista = new System.Windows.Forms.ComboBox();
             this.btnEliminar = new System.Windows.Forms.Button();
             this.label6 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -46,59 +43,29 @@ namespace pryProyecto
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label3 = new System.Windows.Forms.Label();
             this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dgvLista = new System.Windows.Forms.DataGridView();
+            this.Codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Tramite = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            ((System.ComponentModel.ISupportInitialize)(this.dgvCola)).BeginInit();
             this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
             this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
-            // lstCola
+            // lstLista
             // 
-            this.lstCola.FormattingEnabled = true;
-            this.lstCola.Location = new System.Drawing.Point(7, 20);
-            this.lstCola.Name = "lstCola";
-            this.lstCola.Size = new System.Drawing.Size(294, 199);
-            this.lstCola.TabIndex = 0;
-            // 
-            // Nombre
-            // 
-            this.Nombre.HeaderText = "Nombre";
-            this.Nombre.Name = "Nombre";
-            this.Nombre.ReadOnly = true;
-            // 
-            // Codigo
-            // 
-            this.Codigo.HeaderText = "Código";
-            this.Codigo.Name = "Codigo";
-            this.Codigo.ReadOnly = true;
-            // 
-            // dgvCola
-            // 
-            this.dgvCola.AllowUserToAddRows = false;
-            this.dgvCola.AllowUserToDeleteRows = false;
-            this.dgvCola.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvCola.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.Codigo,
-            this.Nombre,
-            this.Tramite});
-            this.dgvCola.Location = new System.Drawing.Point(314, 19);
-            this.dgvCola.Name = "dgvCola";
-            this.dgvCola.ReadOnly = true;
-            this.dgvCola.Size = new System.Drawing.Size(478, 199);
-            this.dgvCola.TabIndex = 1;
-            // 
-            // Tramite
-            // 
-            this.Tramite.HeaderText = "Trámite";
-            this.Tramite.Name = "Tramite";
-            this.Tramite.ReadOnly = true;
+            this.lstLista.FormattingEnabled = true;
+            this.lstLista.Location = new System.Drawing.Point(7, 20);
+            this.lstLista.Name = "lstLista";
+            this.lstLista.Size = new System.Drawing.Size(294, 199);
+            this.lstLista.TabIndex = 0;
             // 
             // groupBox2
             // 
-            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.cboxLista);
             this.groupBox2.Controls.Add(this.btnEliminar);
             this.groupBox2.Controls.Add(this.label6);
             this.groupBox2.Location = new System.Drawing.Point(515, 2);
@@ -108,6 +75,16 @@ namespace pryProyecto
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "Elemento Eliminado";
             // 
+            // cboxLista
+            // 
+            this.cboxLista.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboxLista.FormattingEnabled = true;
+            this.cboxLista.Location = new System.Drawing.Point(52, 134);
+            this.cboxLista.Name = "cboxLista";
+            this.cboxLista.Size = new System.Drawing.Size(221, 21);
+            this.cboxLista.TabIndex = 11;
+            this.cboxLista.SelectedIndexChanged += new System.EventHandler(this.cboxLista_SelectedIndexChanged);
+            // 
             // btnEliminar
             // 
             this.btnEliminar.Location = new System.Drawing.Point(6, 175);
@@ -116,6 +93,7 @@ namespace pryProyecto
             this.btnEliminar.TabIndex = 10;
             this.btnEliminar.Text = "&Eliminar";
             this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
             // 
             // label6
             // 
@@ -166,6 +144,7 @@ namespace pryProyecto
             this.btnAgregar.TabIndex = 3;
             this.btnAgregar.Text = "&Agregar";
             this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click);
             // 
             // txtT
             // 
@@ -201,8 +180,8 @@ namespace pryProyecto
             // 
             // groupBox3
             // 
-            this.groupBox3.Controls.Add(this.dgvCola);
-            this.groupBox3.Controls.Add(this.lstCola);
+            this.groupBox3.Controls.Add(this.dgvLista);
+            this.groupBox3.Controls.Add(this.lstLista);
             this.groupBox3.Location = new System.Drawing.Point(4, 227);
             this.groupBox3.Name = "groupBox3";
             this.groupBox3.Size = new System.Drawing.Size(792, 221);
@@ -210,23 +189,48 @@ namespace pryProyecto
             this.groupBox3.TabStop = false;
             this.groupBox3.Text = "Listado en una Lista y una Grilla";
             // 
+            // dgvLista
+            // 
+            this.dgvLista.AllowUserToAddRows = false;
+            this.dgvLista.AllowUserToDeleteRows = false;
+            this.dgvLista.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLista.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Codigo,
+            this.Nombre,
+            this.Tramite});
+            this.dgvLista.Location = new System.Drawing.Point(308, 20);
+            this.dgvLista.Name = "dgvLista";
+            this.dgvLista.ReadOnly = true;
+            this.dgvLista.Size = new System.Drawing.Size(478, 199);
+            this.dgvLista.TabIndex = 1;
+            // 
+            // Codigo
+            // 
+            this.Codigo.HeaderText = "Código";
+            this.Codigo.Name = "Codigo";
+            this.Codigo.ReadOnly = true;
+            // 
+            // Nombre
+            // 
+            this.Nombre.HeaderText = "Nombre";
+            this.Nombre.Name = "Nombre";
+            this.Nombre.ReadOnly = true;
+            // 
+            // Tramite
+            // 
+            this.Tramite.HeaderText = "Trámite";
+            this.Tramite.Name = "Tramite";
+            this.Tramite.ReadOnly = true;
+            // 
             // pictureBox1
             // 
+            this.pictureBox1.Image = global::pryProyecto.Properties.Resources.Lista_Simple;
             this.pictureBox1.Location = new System.Drawing.Point(4, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(217, 218);
+            this.pictureBox1.Size = new System.Drawing.Size(243, 207);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(52, 134);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(221, 21);
-            this.comboBox1.TabIndex = 11;
             // 
             // frmListaSimple
             // 
@@ -239,12 +243,12 @@ namespace pryProyecto
             this.Controls.Add(this.groupBox3);
             this.Name = "frmListaSimple";
             this.Text = "Estructura de datos lineal: Lista Simple";
-            ((System.ComponentModel.ISupportInitialize)(this.dgvCola)).EndInit();
             this.groupBox2.ResumeLayout(false);
             this.groupBox2.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLista)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -253,11 +257,7 @@ namespace pryProyecto
 
         #endregion
 
-        private System.Windows.Forms.ListBox lstCola;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo;
-        private System.Windows.Forms.DataGridView dgvCola;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Tramite;
+        private System.Windows.Forms.ListBox lstLista;
         private System.Windows.Forms.GroupBox groupBox2;
         private System.Windows.Forms.Button btnEliminar;
         private System.Windows.Forms.Label label6;
@@ -271,6 +271,10 @@ namespace pryProyecto
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.GroupBox groupBox3;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cboxLista;
+        private System.Windows.Forms.DataGridView dgvLista;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Tramite;
     }
 }
